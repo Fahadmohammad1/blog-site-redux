c7a700840d09462b782e6c28c2577581474a2bbc
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-12 21:32:39 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
