67e3f61656bd1bb73fb550ad318a6007573ae19f
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-22 18:36:45 +0600

    data showed on table

--- src/pages/Dashboard/AllBlog.js
+++ src/pages/Dashboard/AllBlog.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Space, Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import loadBlogData from "../../redux/thunk/blogs/fetchBlogs";
 
 const AllBlog = () => {
+  const dispatch = useDispatch();
+  const blogs = useSelector((state) => state.blogs[0]);
+  console.log(blogs);
+
+  useEffect(() => {
+    dispatch(loadBlogData());
+  }, [dispatch]);
+
   const columns = [
     {
       title: "Author Name",
-      dataIndex: "name",
-      key: "name",
-      render: (text) => <a href="/">{text}</a>,
+      dataIndex: "authorName",
+      key: "authorName",
+      render: (authorName) => <a href="/">{authorName}</a>,
     },
     {
       title: "Blog Title",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "title",
+      key: "title",
     },
     {
       title: "Date & Time",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "dateAndTime",
+      key: "date",
     },
 
     {
@@ -26,39 +36,16 @@ const AllBlog = () => {
       render: (_, record) => (
         <Space size="small">
           <Button type="default">Update</Button>
-          <Button type="primary" danger ghost>
+          <Button type="default" danger ghost>
             Delete
           </Button>
         </Space>
       ),
     },
   ];
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["loser"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sidney No. 1 Lake Park",
-      tags: ["cool", "teacher"],
-    },
-  ];
   return (
     <div>
-      <Table columns={columns} dataSource={data} />
+      <Table columns={columns} dataSource={blogs} />
     </div>
   );
 };
