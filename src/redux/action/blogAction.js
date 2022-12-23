5e66a5fb9e917890425c50193be2f2ffdc8198ae
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-23 18:52:42 +0600

    post method added

--- src/redux/action/blogAction.js
+++ src/redux/action/blogAction.js
@@ -1,8 +1,15 @@
-import { GET_CONTENT } from "../actionTypes/actionType";
+import { ADD_CONTENT, GET_CONTENT } from "../actionTypes/actionType";
 
 export const loadBlog = (blogs) => {
   return {
     type: GET_CONTENT,
     payload: blogs,
+  };
+};
+
+export const addBlog = (blog) => {
+  return {
+    type: ADD_CONTENT,
+    payload: blog,
   };
 };
--- src/redux/thunk/blogs/postBlog.js
+++ src/redux/thunk/blogs/postBlog.js
@@ -1,5 +1,26 @@
-const postBlogData = () => {
+import { addBlog } from "../../action/blogAction";
+
+const postBlogData = (blog) => {
   return async (dispatch, getState) => {
-    const data = await fetch();
+    const res = await fetch("https://moon-tech-server-tau.vercel.app/blog", {
+      method: "POST",
+      body: JSON.stringify(blog),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (data.acknowledged) {
+      dispatch(
+        addBlog({
+          _id: data.insertedId,
+          ...blog,
+        })
+      );
+    }
   };
 };
+
+export default postBlogData;
