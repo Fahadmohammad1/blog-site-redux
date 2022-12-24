7f7e0055b0231d6a6bc1e2ed33bdb9d009e690e9
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-24 22:30:45 +0600

    navbar style changed

--- src/layout/Main/Navbar.js
+++ src/layout/Main/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <div>
       <div class="2xl:container 2xl:mx-auto">
@@ -9,7 +11,7 @@ const Navbar = () => {
           <nav class="flex justify-between">
             <div class="flex items-center space-x-3 lg:pr-16 pr-6">
               <Link to="/">
-                <h2 class="font-normal text-2xl leading-6 text-gray-800">
+                <h2 class="font-bold text-2xl leading-6 text-cyan-500">
                   TECHNO BLOG
                 </h2>
               </Link>
@@ -17,16 +19,23 @@ const Navbar = () => {
 
             <ul class="flex flex-auto space-x-2">
               <Link to="/history">
-                <li class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-black border border-white bg-gray-200 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded">
+                <li class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-black border border-white bg-gray-200 cursor-pointer px-3 py-2.5 font-medium text-xs leading-3 shadow-md rounded">
                   History
                 </li>
               </Link>
               <Link to="/dashboard">
-                <li class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-black border border-white bg-gray-200 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded">
+                <li class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-black border border-white bg-gray-200 cursor-pointer px-3 py-2.5 font-medium text-xs leading-3 shadow-md rounded">
                   Dashboard
                 </li>
               </Link>
             </ul>
+            {pathname !== "/dashboard" && (
+              <Link to="/">
+                <span class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-black border border-black bg-cyan-400 cursor-pointer px-3 py-2.5 text-xs leading-3 shadow-md rounded flex flex-auto space-x-2 font-bold">
+                  POST
+                </span>
+              </Link>
+            )}
           </nav>
         </div>
       </div>
